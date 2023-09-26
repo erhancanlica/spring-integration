77e86721b6980a5833daf38fcad3cb781bdc1922
Erhan Canlıca <77679922+erhancanlica@users.noreply.github.com>
2023-09-26 03:19:59 +0300

    Delete target directory

--- target/classes/db/migration/V1.0.10__createUser.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER SPRING_INTEGRATION_API_APP_ADM
-  WITH PASSWORD ${pw.spring_integration_api_app_adm_pwd};
-CREATE USER SPRING_INTEGRATION_API_APP_USR
-  WITH PASSWORD ${pw.spring_integration_api_app_usr_pwd};
